bf45c8fdfc3d03a203c6bed7245f9efe5372253a
Aranel9966 <99angelo.dire@gmail.com>
2023-03-27 14:50:13 +0200

    add components AppBookshelf declared props

---
+++ src/components/AppBookshelf.vue
@@ -0,0 +1,29 @@
+<script>
+export default{
+        data(){
+            return{
+                                  
+            }
+        },
+        props:{
+            img:String,
+            name:String,      
+        }
+    }
+</script>
+<template>
+    <div class="card">
+        <div><img :src="img" alt=""></div>
+        <div>{{name}}</div>
+  
+    </div>
+</template>
+<style lang="scss" scoped>
+    .card{
+        height: 150px;
+        width: 150px;  
+        &:hover{
+
+        }
+    }
+</style>
