4a66a375beb97271f2d9b7bdd2591df849dfe3fc
Aranel9966 <99angelo.dire@gmail.com>
2023-03-28 12:51:36 +0200

    add class active

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,84 +5,91 @@ export default{
     return{
       menuNav:['CHARACTERS','COMICS','MOVIES','TV','GAMES','COLLECTIBLES','VIDEOS','FANS','NEWS','SHOP'],
       comics:[
-  {
-    thumb: "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
-    price: "$19.99",
-    series: "Action Comics",
-    type: "comic book"
-  },
-  {
-    thumb: "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
-    price: "$3.99",
-    series: "American Vampire 1976",
-    type: "comic book"
-  },
-  {
-    thumb: "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
-    price: "$16.99",
-    series: "Aquaman",
-    type: "graphic novel"
-  },
-  {
-    thumb: "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
-    price: "$2.99",
-    series: "Batgirl",
-    type: "comic book"
-  },
-  {
-    thumb: "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
-    price: "$3.99",
-    series: "Batman",
-    type: "comic book"
-  },
-  {
-    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
-    price: "$2.99",
-    series: "Batman Beyond",
-    type: "comic book"
-  },
-  {
-    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
-    price: "$3.99",
-    series: "Batman/Superman",
-    type: "comic book"
-  },
-  {
-    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
-    price: "$4.99",
-    series: "Batman/Superman Annual",
-    type: "comic book"
-  },
-  {
-    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
-    price: "$5.99",
-    series: "Batman: The Joker War Zone",
-    type: "comic book"
-  },
-  {
-    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
-    price: "$6.99",
-    series: "Batman: Three Jokers",
-    type: "comic book"
-  },
-  {
-    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
-    price: "$4.99",
-    series: "Batman: White Knight Presents: Harley Quinn",
-    type: "comic book"
-  },
-  {
-    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
-    price: "$16.99",
-    series: "Catwoman",
-    type: "graphic novel"
-  }
-],
+        {
+          thumb: "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+          price: "$19.99",
+          series: "Action Comics",
+          type: "comic book"
+        },
+        {
+          thumb: "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+          price: "$3.99",
+          series: "American Vampire 1976",
+          type: "comic book"
+        },
+        {
+          thumb: "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+          price: "$16.99",
+          series: "Aquaman",
+          type: "graphic novel"
+        },
+        {
+          thumb: "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+          price: "$2.99",
+          series: "Batgirl",
+          type: "comic book"
+        },
+        {
+          thumb: "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+          price: "$3.99",
+          series: "Batman",
+          type: "comic book"
+        },
+        {
+          thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+          price: "$2.99",
+          series: "Batman Beyond",
+          type: "comic book"
+        },
+        {
+          thumb: "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+          price: "$3.99",
+          series: "Batman/Superman",
+          type: "comic book"
+        },
+        {
+          thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+          price: "$4.99",
+          series: "Batman/Superman Annual",
+          type: "comic book"
+        },
+        {
+          thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+          price: "$5.99",
+          series: "Batman: The Joker War Zone",
+          type: "comic book"
+        },
+        {
+          thumb: "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+          price: "$6.99",
+          series: "Batman: Three Jokers",
+          type: "comic book"
+        },
+        {
+          thumb: "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+          price: "$4.99",
+          series: "Batman: White Knight Presents: Harley Quinn",
+          type: "comic book"
+        },
+        {
+          thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+          price: "$16.99",
+          series: "Catwoman",
+          type: "graphic novel"
+        }
+      ],
+    startIndex:1
       }  
   },
   components:{
     AppBookshelf
-  }
+  },
+  methods: {
+    active(index){
+      this.startIndex=index
+      console.log(this.startIndex)
+    }
+  },
 }
 </script>
 
@@ -93,7 +100,7 @@ export default{
       </div>
       <div class="menu">
         <ul>
-          <li v-for="menuLink in menuNav">{{ menuLink }}</li>
+          <li v-for="(menuLink,index) in menuNav" @click="active(index)" :class="startIndex == index ? 'active' : ''" >{{ menuLink }}</li>
         </ul>
       </div>
     </nav>
@@ -133,12 +140,11 @@ export default{
                 font-size: small;
                 font-weight: bold;
                 padding:  40px 0px;
-                border-bottom: 3px solid transparent;
+                // border-bottom: 3px solid transparent;
 
                 &:hover {
                 cursor: pointer;
                 color: #0282f9;
-                border-bottom-color: #0282f9;
                 }
             }
             }
@@ -176,5 +182,9 @@ export default{
       position: relative;
 
     }
+    .active{
+      color: #0282f9;
+      border-bottom: 3px solid #0282f9;
+      }
     
 </style>
